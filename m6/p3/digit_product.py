2799272ece508cc32fedd4c323a75fd8c46186c5
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-04 11:07:04 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- m6/p3/digit_product.py
+++ m6/p3/digit_product.py
@@ -11,7 +11,10 @@ def main():
         msit = number % 10
         sum1 = sum1 * msit
         number = number // 10
-    print(sum1)
+    if number == 0:
+        print("0")
+    else:
+        print(sum1)
 
 if __name__ == "__main__":
     main()
