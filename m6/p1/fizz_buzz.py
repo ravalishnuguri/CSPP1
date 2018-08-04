03a3ff697712e90fe0ef6fee6b141d58fb07e0e6
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-04 10:08:17 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- m6/p1/fizz_buzz.py
+++ m6/p1/fizz_buzz.py
@@ -11,13 +11,13 @@ def main():
     num = int(input())
     for i in range(1, num+1, 1):
         if(i % 3 == 0 and i % 5 == 0):
-            print("FizzBuzz", end="")
+            print("FizzBuzz")
         elif i % 3 == 0:
-            print("Fizz", end="")
+            print("Fizz")
         elif i % 5 == 0:
-            print("Buzz", end="")
+            print("Buzz")
         else:
-            print(i, end="")
+            print(i)
 
 if __name__ == "__main__":
     main()
