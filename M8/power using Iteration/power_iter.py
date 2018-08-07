5c966c9521416972dcaa06255b3d2dbea022f0a3
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-07 11:49:43 +0530

    recursion

---
+++ M8/power using Iteration/power_iter.py
@@ -0,0 +1,28 @@
+# Exercise: PowerIter
+# Write a Python function, iterPower(base, exp), that takes in two numbers and returns the base^(exp) of given base and exp.
+
+# This function takes in two numbers and returns one number.
+
+
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    # Your code here
+    temp = 1
+    for i in range(exp):
+        temp = temp * base
+    return temp
+
+
+def main():
+
+    data = input()
+    data = data.split()
+    print(iterPower(float(data[0]),int(data[1]))) 
+
+if __name__== "__main__":
+    main()
