af2b672dc24f123d68eb6b2654fb6f7a3b228858
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-16 14:37:10 +0530

    " -> 17 of 20 passed. pylint: 9.90/10 "

--- M14/CodeCampPoker/poker.py
+++ M14/CodeCampPoker/poker.py
@@ -59,17 +59,11 @@ def  is_twopair(hand):
 def is_onepair(hand):
     '''One pair, or simply a pair, is a poker hand containing two cards
     of the same rank and three cards of three other ranks (the kickers)'''
-    flag = 0
     newhand = sorted(sort(hand))
     sethand = set(newhand)
     length1 = len(newhand)
     length2 = len(sethand)
-    maximum = newhand[length1 - 1]
     if length1 - length2 == 1:
-        flag = 1
-    if maximum == max(newhand):
-        flag = 1
-    if flag == 1:
         return True
     return False
 
