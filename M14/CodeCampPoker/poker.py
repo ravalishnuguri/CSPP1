a68e1fa8cdbfe2e096fa20a38fe9048712ef5c29
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-16 12:22:37 +0530

    " -> 17 of 20 passed. pylint: 9.90/10 "

--- M14/CodeCampPoker/poker.py
+++ M14/CodeCampPoker/poker.py
@@ -157,23 +157,24 @@ def hand_rank(hand):
     # any other hand would be the fourth best with the return value 0
     # max in poker function uses these return values to select the best hand
     if is_straight(hand) and is_flush(hand):
-        return 8
+        return 9
     if is_threeofakind(hand) and is_onepair(hand):
-        return 7
+        return 8
     if is_flush(hand):
-        return 6
+        return 7
     if is_straight(hand):
-        return 5
+        return 6
     if is_fourofakind(hand):
-        return 4
+        return 5
     if is_threeofakind(hand):
-        return 3
+        return 4
     if is_twopair(hand):
-        return 2
+        return 3
     if is_onepair(hand):
-        return 1
+        return 2
     if is_highcard(hand):
-        return 0
+        return 1
+    return 0
 
 def poker(hands):
     '''
