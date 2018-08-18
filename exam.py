cac076afa2c967ca124e8f2d4a9bff2ff26b39ef
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-18 12:13:20 +0530

    " -> 0 of 1 passed. pylint: 9.13/10 "

--- exam.py
+++ exam.py
@@ -1,4 +1,5 @@
-st = "deepak"
-if st[2:4:1] != 'ep' or st[::-1] == 'kaped' or st[::] == "deepak":
-	if st[-1:-3:-1] == "ka":
-		print(st[-1:-5:-1])
+dict = {"ab":1, "bc":2, "cd":3, "de":4}
+str = ""
+for k,v in dict:
+	str += k+":"+v
+print(str)
