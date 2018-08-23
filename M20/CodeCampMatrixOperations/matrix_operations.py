3e4af9c9480c8072fa02037593c57aceb6ab9078
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-23 19:16:31 +0530

    " -> 3 of 8 passed. pylint: 9.02/10 "

--- M20/CodeCampMatrixOperations/matrix_operations.py
+++ M20/CodeCampMatrixOperations/matrix_operations.py
@@ -76,8 +76,8 @@ def main():
     mtx1 = read_matrix(number)
     number1 = input().split(',')
     mtx2 = read_matrix(number1)
-    # if mtx1 == None or mtx2 == None:
-    #     return None
+    if mtx1 == None or mtx2 == None:
+        return None
     print(add_matrix(mtx1, mtx2))
     print(mult_matrix(mtx1, mtx2))
 
