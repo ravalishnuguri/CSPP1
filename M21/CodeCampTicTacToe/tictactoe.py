e0fb07497c3cd9d482b9cc1881bacbfb0db1c7e7
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-24 18:03:33 +0530

    " -> 0 of 14 passed. pylint: 5.94/10 "

--- M21/CodeCampTicTacToe/tictactoe.py
+++ M21/CodeCampTicTacToe/tictactoe.py
@@ -1,48 +1,78 @@
-def wincheck(row):
+def loop(set):
+    if 'x' in seti:
+        return 'x'
+    return 'o'
+
+
+def winnercheck(game):
     new1 = set()
     new2 = set()
     new3 = set()
-    for i in range(len(row)):
-        for j in range(len(row[i])):
+    new4 = set()
+    new5 = set()
+    length = len(game)
+    for i in range(length):
+        for j in range(len(game[i])):
             if i == j:
-                new1.add(row[i][j])
-            if i + j == len(row) - 1:
-                new2.add(row[i][j])
+                new1.add(game[i][j])
+            if i + j == (len(game) - 1):
+                new2.add(game[i][j])
+            newt = set(game[i])
+            if len(newt) == 1:
+                if 'x' in newt:
+                    return x
+                return "o"
+            if j == 0:
+                new3.add(game[i][j])
+            if j == 1:
+                new4.add(game[i][j])
+            if j == 2:
+                new5.add(game[i][j])
     if len(new1) == 1:
-        if 'x' in new1:
-            return x
-        return "o"
+        return loop(new1)
     if len(new2) == 1:
-        if 'x' in new2:
-            return "x"
-        return "o"
+        return loop(new2)
+    if len(new3) == 1:
+        return loop(new3)
+    if len(new4) == 1:
+        return loop(new4)
+    if len(new5) == 1:
+        return loop(new5)
     return "draw"
 
-
 def gamerules(row):
-    for i in row:
+    palyer1 = 'x'
+    player2 = 'o'
+    countofx = 0
+    countofo = 0
+    countofdot = 0
+    for index in row:
         if len(i) != 3:
             return "invalid game"
-    countofx = index.count("x")
-    countofo = index.count("o")
-    countofdot = index.count(".")
-    if sum(countofx + countofo + countofdot) != 9:
+        if palyer1 in index:
+            countofx += index.count(palyer1)
+        if player2 in index:
+            countofo = index.count(player2)
+        if '.' in index:
+            countofdot = index.count('.')
+    if countofx + countofo + countofdot != 9:
         return "invalid input"
     if abs(countofx - countofo) != 1:
         return "invalid game"
+    return winnercheck(game)
 
 
 
 def main():
     '''main function for the tic-tac-toe game'''
     row = []
-    for i in range(3):
+    for _ in range(3):
         matrix = input()
-        matrix = list(map(str, matrix.split()))
+        matrix = list(map(str, matrix.split(' ')))
         row.append(matrix)
     # print(row)
-    gamerules(row)
-    print(wincheck(row))
+    row = gamerules(row)
+    print(row)
 
 
 
