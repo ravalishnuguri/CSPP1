051d5c3fad404ce4e9a23e2ae3d1adb22e68e685
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-25 10:40:33 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- M22/assignment2/clean_input.py
+++ M22/assignment2/clean_input.py
@@ -6,7 +6,7 @@ import re
 def clean_string(string):
     '''function for cleaning a string'''
     newstring = string.lower().strip()
-    newstring1 = re.sub('[^a-z]', '', newstring)
+    newstring1 = re.sub('[^a-z0-9]', '', newstring)
     # return newstring1
     newstring2 = re.sub(r"\s+", "", newstring1,)
     return newstring2
