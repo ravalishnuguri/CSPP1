b1db6eaf28f6947fea7fbdcba301f8d9dcc4ebaf
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-25 12:39:14 +0530

    " -> 4 of 4 passed. pylint: 10.00/10 "

--- M22/assignment3/tokenize.py
+++ M22/assignment3/tokenize.py
@@ -3,7 +3,20 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 import re
-import collections
+
+
+def word_count(string):
+    '''for counting the words'''
+    counts = dict()
+    words = string.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
 def tokenize(string):
     '''we are counting the number of words in the string'''
     newtext = ""
@@ -11,17 +24,17 @@ def tokenize(string):
     newtext = newtext.strip()
     newstring1 = re.sub('[^a-zA-Z0-9]', ' ', newtext)
     # print(list(newstring1))
-    li = list(newstring1.split(" "))
-    print(li)
+    # li = list(newstring1.split(" "))
+    # print(li)
     # list1 = []
     # for word in newstring1:
     #   list1.append(word)
     # print(li)
 
-    freq1 = collections.Counter(li)
+    # freq1 = collections.Counter(li)
     # for word in freq1:
 
-    print(freq1)
+    print(word_count(newstring1))
 
 
 def main():
