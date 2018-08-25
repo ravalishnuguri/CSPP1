cdf7cb77dda2d1ea232852c25f6c3edbcbb36740
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-08-25 12:15:21 +0530

    " -> 0 of 26 passed. pylint: 10.00/10 "

--- M22/Check Sudoku/check_sudoku.py
+++ M22/Check Sudoku/check_sudoku.py
@@ -33,7 +33,7 @@ def main():
         row = input().split(' ')
         sudoku.append(row)
     # call solution function and print result to console
-    print(sudoku)
+    # print(sudoku)
     print(check_sudoku(sudoku))
 
 if __name__ == '__main__':
